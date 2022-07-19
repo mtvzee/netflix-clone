5201e125395e36edce8ea09a02af7655807d924e
mtvzee <sample@aaaaa.com>
2022-07-19 16:31:23 +0900

    atomsフォルダを削除

--- atoms/modalAtom.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from 'recoil';
-
-export const showModalState = atom({ key: 'showModalState', default: false });
-
-// export const movieState = atom<Movie | DocumentData | null>({ key: 'movieState', default: null });
