80dd82550c15daa16df4761ea9a684a7ed2ffbf5
mtvzee <pgzv01@gmail.com>
2023-03-10 13:38:30 +0900

    NextJsのバージョンアップに伴う記述の変更とCSS Modulesへの移行

--- pages/signup/registration.tsx
+++ pages/signup/registration.tsx
@@ -2,10 +2,11 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { auth } from '../../firebase';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import styles from '../../styles/pages/Registration.module.css';
 
 type FormInput = {
   email: string;
@@ -37,67 +38,70 @@ const Registration = () => {
   };
 
   return (
-    <div className="min-h-screen text-black bg-white">
+    <div className={styles.container}>
       <Head>
-        <title>Netflix</title>
+        <title>NetflixClone</title>
       </Head>
-      <header className="flex justify-between px-5 py-1 border-b md:px-7 md:py-3">
-        <div
-          className="relative h-20 cursor-pointer w-44 "
-          onClick={() => router.push('/signup')}
-        >
+      <header className={styles.header}>
+        <div className={styles.logoImgWrapper}>
           <Image
             src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
             alt="logo"
-            layout="fill"
-            objectFit="contain"
+            fill
+            className={styles.logoImg}
           />
         </div>
-        <Link href="/login">
-          <a className="flex items-center text-lg font-bold md:text-2xl hover:underline">
-            ログイン
-          </a>
+        <Link href="/login" className={styles.login}>
+          ログイン
         </Link>
       </header>
-      <div className="max-w-lg px-4 pt-16 mx-auto space-y-4">
-        <h1 className="text-4xl font-bold max-w-[80%] leading-snug">
+
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.regBox}>
+        <h1 className={styles.title}>
           パスワードを作成してメンバーシップを始めてください。
         </h1>
-        <p className="text-xl text-neutral-600">あともう少しで完了!</p>
-        <p className="text-xl text-neutral-600">登録はとても簡単です。</p>
-        <form
-          className="flex flex-col space-y-6"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className="flex flex-col space-y-3">
+        <p className={styles.text}>あともう少しで完了!</p>
+        <p className={styles.text}>登録はとても簡単です。</p>
+        <div className={styles.inputWrapper}>
+          <div className={styles.inputLabel}>
             <input
               type="email"
-              className="w-full px-3 py-5 border-2 border-gray-300 rounded-sm focus:outline-blue-400"
+              placeholder=" "
+              className={`${styles.textField} ${
+                errors.email && styles.errorInput
+              }`}
               {...register('email', { required: true })}
             />
-            <label>
-              <input
-                type="password"
-                placeholder="パスワードを追加してください"
-                className={`w-full px-3 py-5 border-2 border-gray-300 rounded-sm focus:outline-blue-400 errors.password && 'border-red-600 focus:outline-none'`}
-                {...register('password', { required: true })}
-              />
-              {errors.password && (
-                <p className="text-red-700 ">パスワードの入力が必要です。</p>
-              )}
-            </label>
-            <label className="flex items-center space-x-2">
-              <input type="checkbox" className="w-8 h-8 accent-blue-600" />
-              <span className="text-xl text-neutral-600">
-                キャンペーン案内メールを希望しない
-              </span>
+            <label className={styles.placeholderLabel}>メールアドレス</label>
+          </div>
+          {errors.email && (
+            <p className={styles.errorMsg}>メールアドレスの入力が必要です。</p>
+          )}
+        </div>
+        <div className={styles.inputWrapper}>
+          <div className={styles.inputLabel}>
+            <input
+              type="password"
+              placeholder=" "
+              className={`${styles.textField} ${
+                errors.password && styles.errorInput
+              }`}
+              {...register('password', { required: true })}
+            />
+            <label className={styles.placeholderLabel}>
+              パスワードを追加してください
             </label>
           </div>
-          <button className="py-4 text-3xl text-white bg-red-600 rounded-md">
-            続ける
-          </button>
-        </form>
-      </div>
+          {errors.password && (
+            <p className={styles.errorMsg}>パスワードの入力が必要です。</p>
+          )}
+        </div>
+        <div className={styles.checkboxWrapper}>
+          <input type="checkbox" id="checkbox" className={styles.checkbox} />
+          <label htmlFor="checkbox">キャンペーン案内メールを希望しない</label>
+        </div>
+        <button className={styles.regBtn}>続ける</button>
+      </form>
     </div>
   );
 };
